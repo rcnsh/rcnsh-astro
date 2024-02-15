0578e91096f639f8470a0e74ba47874f2b34c4ca
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-02-15 18:00:42 +0000

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
@@ -8,5 +8,5 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: vercel()
+  adapter: vercel(),
 });
