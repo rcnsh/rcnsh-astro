d404df443292ce3d47eff5214ebbb137edcb2748
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-02-21 16:49:56 +0000

    try static rendering

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rcn.sh",
   integrations: [tailwind(), react(), sitemap()],
-  output: "server",
-  adapter: vercel()
+  output: "static",
 });
