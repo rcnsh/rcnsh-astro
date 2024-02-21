9bc5a07cd4eb2ab8e4098a95e9929e89ad636e0a
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-02-21 16:41:55 +0000

    add sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  output: "server",
-  adapter: vercel(),
+  site: "https://rcn.sh",
+  integrations: [tailwind(), react(), sitemap()],
+  output: "hybrid",
+  adapter: vercel()
 });
