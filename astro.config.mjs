48d0d69ad30c71d21201c4a3ea02f271315afa84
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-02-21 17:00:24 +0000

    add redirect test

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rcn.sh",
+  redirects: {"/github": "https://github.com/RCNOverwatcher"},
   integrations: [tailwind(), react(), sitemap()],
   output: "server",
   adapter: vercel()
