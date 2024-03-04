0241aa785fb472c4b4172a353ad7e84b168ed5b5
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-04 16:57:44 +0000

    add type declarations

--- src/pages/spotify.json.ts
+++ src/pages/spotify.json.ts
@@ -1,81 +1,5 @@
 import { z } from "zod";
 
-interface ExternalUrls {
-  spotify: string;
-}
-
-interface Artist {
-  external_urls: ExternalUrls;
-  href: string;
-  id: string;
-  name: string;
-  type: string;
-  uri: string;
-}
-
-interface Album {
-  album_type: string;
-  artists: Artist[];
-  available_markets: string[];
-  external_urls: ExternalUrls;
-  href: string;
-  id: string;
-  images: { height: number; url: string; width: number }[];
-  name: string;
-  release_date: string;
-  release_date_precision: string;
-  total_tracks: number;
-  type: string;
-  uri: string;
-}
-
-interface ExternalIds {
-  isrc: string;
-}
-
-interface Item {
-  album: Album;
-  artists: Artist[];
-  available_markets: string[];
-  disc_number: number;
-  duration_ms: number;
-  explicit: boolean;
-  external_ids: ExternalIds;
-  external_urls: ExternalUrls;
-  href: string;
-  id: string;
-  is_local: boolean;
-  name: string;
-  popularity: number;
-  preview_url: string;
-  track_number: number;
-  type: string;
-  uri: string;
-}
-
-interface Context {
-  external_urls: ExternalUrls;
-  href: string;
-  type: string;
-  uri: string;
-}
-
-interface Actions {
-  disallows: {
-    pausing: boolean;
-  };
-}
-
-interface SpotifyData {
-  timestamp: number;
-  context: Context;
-  progress_ms: number;
-  item: Item;
-  currently_playing_type: string;
-  actions: Actions;
-  is_playing: boolean;
-}
-
 export async function getAccessToken(): Promise<{ access_token: string }> {
   const refreshToken = import.meta.env.SPOTIFY_REFRESH_TOKEN;
   const clientId = import.meta.env.SPOTIFY_CLIENT_ID;
@@ -104,7 +28,7 @@ export async function getAccessToken(): Promise<{ access_token: string }> {
   return { access_token };
 }
 
-async function GetSpotifyData() {
+async function GetSpotifyData(): Promise<songData> {
   try {
     const { access_token } = await getAccessToken();
 
---
+++ src/types/spotify.d.ts
@@ -0,0 +1,82 @@
+interface ExternalUrls {
+    spotify: string;
+  }
+  
+  interface Artist {
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+  }
+  
+  interface Album {
+    album_type: string;
+    artists: Artist[];
+    available_markets: string[];
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    images: { height: number; url: string; width: number }[];
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    total_tracks: number;
+    type: string;
+    uri: string;
+  }
+  
+  interface ExternalIds {
+    isrc: string;
+  }
+  
+  interface Item {
+    album: Album;
+    artists: Artist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: ExternalIds;
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track_number: number;
+    type: string;
+    uri: string;
+  }
+  
+  interface Actions {
+    disallows: {
+      pausing: boolean;
+    };
+  }
+  
+  interface SpotifyData {
+    timestamp: number;
+    context: {
+      external_urls: ExternalUrls;
+    href: string;
+    type: string;
+    uri: string;
+    };
+    progress_ms: number;
+    item: Item;
+    currently_playing_type: string;
+    actions: Actions;
+    is_playing: boolean;
+  }
+  
+  interface songData {
+    isPlaying: boolean;
+    title: string;
+    artist: string;
+    album: string;
+    albumImageUrl: string;
+    songUrl: string;
+  }
