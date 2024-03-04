7009836912d2d581b0c9238b541ac2cd9246ed4b
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-04 17:28:16 +0000

    use squooshimages?

--- astro.config.ts
+++ astro.config.ts
@@ -1,14 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, squooshImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rcn.sh",
   integrations: [tailwind(), react(), sitemap()],
+  image: {
+    service: squooshImageService(),
+  },
   output: "server",
   adapter: vercel({
     isr: {
