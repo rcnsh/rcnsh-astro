26ff1d1a3f428b98763ef3e046d1b6025c7801f5
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-11 21:21:22 +0000

    add new background and spotify endpoint

--- src/pages/api/spotify.ts
+++ src/pages/api/spotify.ts
@@ -31,11 +31,58 @@ export async function getAccessToken(): Promise<{ access_token: string }> {
   return { access_token };
 }
 
-export async function getTopTracks() {
+export async function getTopTracksMedium() {
   const { access_token } = await getAccessToken();
 
   const response = await fetch(
     "https://api.spotify.com/v1/me/top/tracks?time_range=medium_term",
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    },
+  ).then((res) => res.json());
+
+  const { items } = z
+    .object({
+      items: z.array(
+        z.object({
+          artists: z.array(
+            z.object({
+              name: z.string(),
+            }),
+          ),
+          album: z.object({
+            name: z.string(),
+            images: z.array(
+              z.object({
+                url: z.string(),
+              }),
+            ),
+          }),
+          external_urls: z.object({
+            spotify: z.string(),
+          }),
+          name: z.string(),
+        }),
+      ),
+    })
+    .parse(response);
+
+  return items.slice(0, 12).map((item) => ({
+    artists: item.artists,
+    songUrl: item.external_urls.spotify,
+    title: item.name,
+    album: item.album.name,
+    image: item.album.images[0].url,
+  }));
+}
+
+export async function getTopTracksShort() {
+  const { access_token } = await getAccessToken();
+
+  const response = await fetch(
+    "https://api.spotify.com/v1/me/top/tracks?time_range=short_term",
     {
       headers: {
         Authorization: `Bearer ${access_token}`,
@@ -119,11 +166,13 @@ export async function getTopArtists() {
 }
 
 export const GET: APIRoute = async () => {
-  const tracks = await getTopTracks();
+  const tracksMedium = await getTopTracksMedium();
+  const tracksShort = await getTopTracksShort();
   const artists = await getTopArtists();
   return new Response(
     JSON.stringify({
-      tracks: tracks,
+      tracksMedium: tracksMedium,
+      tracksShort: tracksShort,
       artists: artists,
     }),
     {
