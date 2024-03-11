57a2c44f32d1f6b96da761d06f69ba538e8f9d41
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-11 12:02:59 +0000

    lazy load test

--- src/components/InfiniteSteamCardsAll.tsx
+++ src/components/InfiniteSteamCardsAll.tsx
@@ -102,6 +102,7 @@ export const InfiniteMovingCardsAll = ({
                 className={"w-150 h-225 rounded-lg pointer-events-none"}
                 width={150}
                 height={225}
+                loading="lazy"
               />
             </a>
             <p
