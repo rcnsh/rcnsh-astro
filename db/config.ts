8d55312f1465536c39760e40f85d76f18a0104a5
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-12 18:30:15 +0000

    add guestbook table

--- db/config.ts
+++ db/config.ts
@@ -1,7 +1,16 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
+
+const Guestbook = defineTable({
+  columns: {
+    author: column.text(),
+    message: column.text(),
+    published: column.date({ default: new Date()}),
+  }
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { Guestbook }
 });
+
 
