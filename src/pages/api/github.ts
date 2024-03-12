6e8a9109a259150c70ca0691bc64d10e01f0d430
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-12 18:37:35 +0000

    prettier

--- src/pages/api/github.ts
+++ src/pages/api/github.ts
@@ -1,21 +1,20 @@
 import type { APIRoute } from "astro";
 import { Octokit } from "@octokit/core";
 
-
 const octokit = new Octokit({
-    auth: import.meta.env.GITHUB_TOKEN,
-  })
+  auth: import.meta.env.GITHUB_TOKEN,
+});
 
-  export const GET: APIRoute = async () => {
-    const response = await octokit.request('GET /users/RCNOverwatcher/repos', {
-        username: 'RCNOverwatcher',
-        headers: {
-          'X-GitHub-Api-Version': '2022-11-28'
-        }
-      });
-    return new Response(JSON.stringify(response.data), {
-      headers: {
-        "content-type": "application/json",
-      },
-    });
-  }
+export const GET: APIRoute = async () => {
+  const response = await octokit.request("GET /users/RCNOverwatcher/repos", {
+    username: "RCNOverwatcher",
+    headers: {
+      "X-GitHub-Api-Version": "2022-11-28",
+    },
+  });
+  return new Response(JSON.stringify(response.data), {
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+};
