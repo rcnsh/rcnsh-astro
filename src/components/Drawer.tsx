9416b673af33d04c864f6d19327b4dcc4578ab9c
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-13 16:15:39 +0000

    remove svg from drawer

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -156,20 +156,6 @@ export default function MobileDrawer() {
                             Guestbook
                           </span>
                         </span>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                        >
-                          <path d="M3 3m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v14a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
-                          <path d="M10 8h3a1 1 0 0 1 1 1v2a1 1 0 0 1 -1 1h-2a1 1 0 0 0 -1 1v2a1 1 0 0 0 1 1h3"></path>
-                        </svg>
                       </span>
                     </a>
                   </div>
