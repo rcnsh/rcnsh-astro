195d90cd4dd23ce920ba192c0710c56dd8747561
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-14 18:48:11 +0000

    exclude signout

--- astro.config.ts
+++ astro.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   output: "server",
   adapter: vercel({
     isr: {
-      exclude: [ "/api/**", "/api/auth/callback/github" ]
+      exclude: [ "/api/auth/callback/github", "/api/auth/signout"]
     },
     webAnalytics: {
       enabled: true,
