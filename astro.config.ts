00a676e7b00424ce8047af1e543c034c49ee76e0
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-15 16:53:42 +0000

    exclude nowplaying from ISR

--- astro.config.ts
+++ astro.config.ts
@@ -25,6 +25,7 @@ export default defineConfig({
   adapter: vercel({
     isr: {
       exclude: [
+        "/api/nowPlaying",
         "/api/auth/callback/github",
         "/api/auth/signout",
         "/api/auth/csrf",
