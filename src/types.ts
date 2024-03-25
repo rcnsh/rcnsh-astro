dee8cae3c0be847d83ad9002277d3bbf99f07123
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-25 19:19:40 +0000

    add more credit and, isolate types and add seperate api for all song info

---
+++ src/types.ts
@@ -0,0 +1,75 @@
+interface ExternalUrls {
+    spotify: string;
+  }
+  
+  interface Artist {
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    name: string;
+    type: string;
+    uri: string;
+  }
+  
+  interface Album {
+    album_type: string;
+    artists: Artist[];
+    available_markets: string[];
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    images: { height: number; url: string; width: number }[];
+    name: string;
+    release_date: string;
+    release_date_precision: string;
+    total_tracks: number;
+    type: string;
+    uri: string;
+  }
+  
+  interface ExternalIds {
+    isrc: string;
+  }
+  
+  interface Item {
+    album: Album;
+    artists: Artist[];
+    available_markets: string[];
+    disc_number: number;
+    duration_ms: number;
+    explicit: boolean;
+    external_ids: ExternalIds;
+    external_urls: ExternalUrls;
+    href: string;
+    id: string;
+    is_local: boolean;
+    name: string;
+    popularity: number;
+    preview_url: string;
+    track_number: number;
+    type: string;
+    uri: string;
+  }
+  
+  interface Context {
+    external_urls: ExternalUrls;
+    href: string;
+    type: string;
+    uri: string;
+  }
+  
+  interface Actions {
+    disallows: {
+      pausing: boolean;
+    };
+  }
+  
+ export interface SpotifyData {
+    timestamp: number;
+    context: Context;
+    progress_ms: number;
+    item: Item;
+    currently_playing_type: string;
+    actions: Actions;
+    is_playing: boolean;
+  }
