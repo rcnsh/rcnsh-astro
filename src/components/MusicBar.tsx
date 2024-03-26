77b978ee2d70a43ce75b63ccdd4ce236810c670f
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-03-26 17:32:27 +0000

    rollback

--- src/components/MusicBar.tsx
+++ src/components/MusicBar.tsx
@@ -11,53 +11,48 @@ export default function MusicBar({
   progress: number;
   isPlaying: boolean;
 }) {
+  async function updateUI() {
+    await fetch("https://rcn.sh/api/nowPlaying")
+      .then((response) => response.json() as Promise<songData>)
+      .then((data) => {
+        const image = document.getElementById(
+          "spotify-image",
+        ) as HTMLImageElement;
+
+        image.src = data.albumImageUrl;
+        document.getElementById("nowPlayingTitle")!.innerText = data.title;
+        document.getElementById("nowPlayingArtist")!.innerText = data.artist;
+        document.getElementById("nowPlayingAlbum")!.innerText = data.album;
+
+        setDuration(data.songLength);
+        setProgress(data.songProgress);
+        setIsSongPlaying(data.isPlaying);
+        if (image) {
+          Vibrant.from(image.src)
+            .getPalette()
+            .then((palette) => {
+              const vibrantColour = palette.Vibrant!.hex;
+              const mutedColour = palette.Muted!.hex;
+              const darkMutedColour = palette.DarkMuted!.hex;
+
+              const element = document.getElementById("spotify-background");
+              element!.style.background = `linear-gradient(-45deg, ${vibrantColour}, ${mutedColour}, ${darkMutedColour}`;
+              element!.style.boxShadow = `0 0 15px 10px ${darkMutedColour}`;
+            });
+        }
+      });
+  }
+  const [isSongPlaying, setIsSongPlaying] = useState<boolean>(isPlaying);
   const [currentProgress, setProgress] = useState(progress);
   const [currentSongDuration, setDuration] = useState(duration);
-  const [currentIsPlaying, setIsPlaying] = useState(isPlaying);
   const progressPercent = (currentProgress / currentSongDuration) * 100;
 
   useEffect(() => {
     let interval: ReturnType<typeof setInterval>;
-    if (currentIsPlaying) {
+    if (isSongPlaying) {
       interval = setInterval(() => {
         setProgress((currentProgress) => {
           if (currentProgress < currentSongDuration) {
-            fetch("https://rcn.sh/api/nowPlaying")
-              .then((response) => response.json() as Promise<songData>)
-              .then((data) => {
-                const image = document.getElementById(
-                  "spotify-image",
-                ) as HTMLImageElement;
-
-                image.src = data.albumImageUrl;
-                document.getElementById("nowPlayingTitle")!.innerText =
-                  data.title;
-                document.getElementById("nowPlayingArtist")!.innerText =
-                  data.artist;
-                document.getElementById("nowPlayingAlbum")!.innerText =
-                  data.album;
-
-                setDuration(data.songLength);
-                setProgress(data.songProgress);
-                setIsPlaying(data.isPlaying);
-                if (image) {
-                  Vibrant.from(image.src)
-                    .getPalette()
-                    .then((palette) => {
-                      const vibrantColour = palette.Vibrant!.hex;
-                      const mutedColour = palette.Muted!.hex;
-                      const darkMutedColour = palette.DarkMuted!.hex;
-
-                      const element =
-                        document.getElementById("spotify-background");
-                      element!.style.background = `linear-gradient(-45deg, ${vibrantColour}, ${mutedColour}, ${darkMutedColour}`;
-                      element!.style.boxShadow = `0 0 15px 10px ${darkMutedColour}`;
-                    });
-                }
-              })
-              .catch((error) => {
-                console.error("Error:", error);
-              });
             return currentProgress + 1000;
           } else {
             clearInterval(interval);
@@ -66,10 +61,13 @@ export default function MusicBar({
         });
       }, 1000);
     }
+    const updateUIInterval = setInterval(updateUI, 5000);
+
     return () => {
       if (interval) {
         clearInterval(interval);
       }
+      clearInterval(updateUIInterval);
     };
   }, [isPlaying, currentSongDuration]);
 
