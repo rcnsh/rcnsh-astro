ef771d9e2cb8ef6fe3a12f97fdaf089898cbbf41
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-04-03 15:00:38 +0100

    change error again

--- src/pages/api/guestbook-status.ts
+++ src/pages/api/guestbook-status.ts
@@ -1,11 +1,11 @@
 import type { APIRoute } from "astro";
- 
+
 export const POST: APIRoute = async ({ cookies, redirect }) => {
   cookies.set("status", "Too many requests, try again later", {
     httpOnly: true,
     sameSite: "strict",
     path: "/",
   });
- 
+
   return redirect("/");
 };
