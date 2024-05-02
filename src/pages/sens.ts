291547e58007ec843e36da8701e65da150c55414
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-05-02 13:02:15 +0100

    add sens

---
+++ src/pages/sens.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = () => {
+  return new Response(
+    "1600dpi 0.125 ingame",
+    {
+      headers: {
+        "Content-Type": "text/plain; charset=utf-8",
+      },
+    },
+  );
+};
