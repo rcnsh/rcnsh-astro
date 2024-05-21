eef6c65efa7e1ac723651f061fa610a99a44ecb7
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2024-05-21 19:36:43 +0100

    refactor sidebar codebase

---
+++ src/lib/constants.ts
@@ -0,0 +1,22 @@
+export const LINKS = [
+    {
+      href: "/",
+      label: "Home",
+    },
+    {
+      href: "/games",
+      label: "Games",
+    },
+    {
+      href: "/music",
+      label: "Music",
+    },
+    {
+      href: "/guestbook",
+      label: "Guestbook",
+    },
+    {
+      href: "/files",
+      label: "Files",
+    },
+  ];
