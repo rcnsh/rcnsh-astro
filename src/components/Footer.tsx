37d73ad29489666cd35331620e493f2cc44b37e2
Jacob Wiltshire <jacobjameswiltshire@protonmail.com>
2025-01-03 22:40:22 +0000

    change year

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ export default function Footer({ children }: { children: React.ReactNode }) {
       <hr className="bg-[#c5c0b8] h-[1px] w-full" />
       <div className="mt-4">
         <div className="flex flex-row text-[#c5c0b8] font-mono text-center justify-between text-sm lg:text-lg lg:px-12">
-          <span className="w-[20%]"> rcn.sh © 2024 </span>
+          <span className="w-[20%]"> rcn.sh © {new Date().getFullYear()} </span>
           <Joke />
           {children}
         </div>
